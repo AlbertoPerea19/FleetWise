19d88154ab0da4d7162cd50d7ce3ec107f37e332
JulianChan123 <a20216390@alumnos.uady.mx>
2024-05-19 21:52:32 -0600

    yeah

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ import * as cors from 'cors';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cors({
-    origin: 'http://localhost',
+    origin: 'http://localhost:4200',
     credentials: true,
   }));
   await app.listen(3000);
